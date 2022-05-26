1336d03f24b2ca37239ae4118125cc261fc96b79
EresK <e.kuular@g.nsu.ru>
2022-05-27 02:25:15 +0700

    add new version of window

---
+++ WindowAppNew/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace WindowAppNew;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void FileLoadClick(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+
+        if (dialog.ShowDialog() == true)
+        {
+            string filename = dialog.FileName;
+            userControl.Content = new MenuControl(filename);
+        }
+    }
+
+    public void DropModel(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            object? files = e.Data.GetData(DataFormats.FileDrop);
+            if (files is string[] names)
+            {
+                string filename = names[0];
+                userControl.Content = new MenuControl(filename);
+            }
+        }
+    }
+}
+
